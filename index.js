6dc7aab7482df74c34af271bfaa01e507d1385c7
FultonDrew <andrewgfulton@gmail.com>
2019-01-15 10:46:34 -0500

    finished mobile responsive

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
 // Scroll back to top feature
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-    document.getElementById("backTopBtn").style.opacity= "100";
+  if (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) {
+    document.getElementById("navbar").style.opacity= "100";
+    document.getElementById("navbar").classList = "sticky";
+    // document.getElementsByClassName("sticky").style.opacity= "100";
   } else {
-    document.getElementById("backTopBtn").style.opacity = "0";
+    document.getElementById("navbar").style.opacity= "0";
+    // document.getElementsByClassName("sticky").style.opacity= "0";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
 
-// Nav icon click listener
-$(document).ready(function(){
-	$('#nav-icon1').click(function(){
-		$(this).toggleClass('open');
-	});
-});
-
-/* Set the width of the side navigation to 250px */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
 
